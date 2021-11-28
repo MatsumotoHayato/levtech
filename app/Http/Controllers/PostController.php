937c1779bd08ee4e2479d2113f8387def83fff22
k458253 <k458253@kansai-u.ac.jp>
2021-11-29 01:44:16 +0900

    [add]ブログ投稿作成画面（カリキュラム08-4）

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Post;
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
@@ -21,5 +22,17 @@ class PostController extends Controller
     public function show(Post $post)
     {
         return view('show')->with(['post' => $post]);
+    }
+
+    public function create()
+    {
+        return view('create');
+    }
+
+    public function store(Post $post, PostRequest $request)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/'. $post->id);
     }
 }
